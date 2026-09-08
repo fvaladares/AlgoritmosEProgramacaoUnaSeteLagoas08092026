e7786340775e7a8511dc027852d2817d864749a3
Fabricio Valadares <fabricio.valadares@gmail.com>
2026-09-08 20:13:02 -0300

    Código finalizado.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,5 +13,55 @@ public class Main {
         5. Imprimir os resultados.
          */
         Scanner entrada = new Scanner(System.in);
+        int codigoProduto;
+        int quantidadeProduto;
+        double valorUnitario = 1;
+        double valorTotalNF;
+        double desconto = 0;
+        double valorTotalComDesconto;
+        double valorDoDesconto;
+        System.out.println("Programa emissor de NF");
+        System.out.print("Informe o código do produto: ");
+        codigoProduto = entrada.nextInt();
+
+        System.out.print("Informe a quantidade do produto: ");
+        quantidadeProduto = entrada.nextInt();
+
+        // Estrutura condicional que determina o valor unitário.
+        if (codigoProduto >= 1 && codigoProduto <= 10) {
+            valorUnitario = 10;
+        } else if (codigoProduto > 10 && codigoProduto <= 20) {
+            valorUnitario = 15;
+        } else if (codigoProduto > 20 && codigoProduto <= 30) {
+            valorUnitario = 20;
+        } else if (codigoProduto > 30 && codigoProduto <= 40) {
+            valorUnitario = 30;
+        }
+
+        valorTotalNF = valorUnitario * quantidadeProduto;
+
+        if (valorTotalNF < 250)
+            desconto = .05;
+        else if (valorTotalNF > 250 && valorTotalNF < 500)
+            desconto = .1;
+        else if (valorTotalNF > 500)
+            desconto = .15;
+
+        valorDoDesconto = (valorTotalNF * desconto);
+        valorTotalComDesconto = valorTotalNF - valorDoDesconto;
+
+        System.out.println();
+        System.out.println(".:|NF Una|:.");
+        System.out.printf("\tValor unitário R$ %.2f\n",
+                valorUnitario);
+        System.out.printf("\tValor total da NF sem desconto R$ %.2f\n",
+                valorTotalNF);
+        System.out.printf("\tValor do desconto concedido: R$ %.2f\n",
+                valorDoDesconto);
+        System.out.printf("\tValor final da NF (já com desconto): R$ %.2f\n",
+                valorTotalComDesconto);
+        System.out.println();
+        System.out.println("Até mais!! \uD83D\uDE01 ❤\uFE0F");
+
     }
 }
