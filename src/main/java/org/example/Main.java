bb7542382f3a8f4d3fd226f02406800a7278589f
Fabricio Valadares <fabricio.valadares@gmail.com>
2026-09-08 19:49:21 -0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        1. Criar uma estrutura condicional, para saber o valor unitário com base no código fornecido
+        2. Calcular o valor total da NF.
+        3. Criar uma estrutura condicional para determinar o valor do desconto com base no valor total da compra.
+        4. Calcular o preço final
+        5. Imprimir os resultados.
+         */
+        Scanner entrada = new Scanner(System.in);
+    }
+}
